a54499224aa26aff30688dce4fe78f1911425573
Gildas DZINGA <gildas.dzinga@gmail.com>
2018-03-12 15:15:32 +0100

    Version plus propres ?

---
+++ src/fr/epsi/jenkins/b3/EcoleEnum.java
@@ -0,0 +1,9 @@
+package fr.epsi.jenkins.b3;
+
+public enum EcoleEnum {
+	EPSI,
+	IFAG,
+	SUPDECOM,
+	IEFT
+}
+// TEST Andy
---
+++ src/fr/epsi/jenkins/b3/NiveauEnum.java
@@ -0,0 +1,9 @@
+package fr.epsi.jenkins.b3;
+
+public enum NiveauEnum {
+	B1,
+	B2,
+	B3,
+	I4,
+	I5
+}
